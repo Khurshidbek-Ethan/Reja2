63148bf312bcbc9e8c8c397abc13f202183cde77
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-12 15:32:44 +0900

    fix:refactor

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/author',(req,res)=>{
 
 
 app.get("/",function(req,res){
-    res.render("harid");
+    res.render("reja");
     });
 
 
@@ -45,5 +45,6 @@ app.get("/",function(req,res){
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT,function (){
-    console.log(`The server is running succefully on port:${PORT}`);
+    console.log(`The server is running succefully on port:${PORT}, http://localhost:${PORT}`
+    );
 }) 
